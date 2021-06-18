84bb46997792042950cc38fd85a9d4bde3c66e9f
Shoaibashk <shoaibashk.2000@gmail.com>
2021-06-18 20:33:48 +0530

    main.c Added

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d",&n);
+
+    if (n == 1)
+    {
+        printf("hello");
+    }
+    else if (n == 2)
+    {
+        printf("world");
+    }
+   
+    
+    return 0;
+}
