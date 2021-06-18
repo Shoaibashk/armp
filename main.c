7d9905c094005bcfd17b21e0d162b05c46b3f42d
Shoaibashk <shoaibashk.2000@gmail.com>
2021-06-18 20:35:28 +0530

    First Code

--- main.c
+++ main.c
@@ -4,17 +4,16 @@ int main()
 {
     int n;
 
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     if (n == 1)
     {
-        printf("hello");
+        printf("hello\n");
     }
     else if (n == 2)
     {
-        printf("world");
+        printf("world\n");
     }
-   
-    
+
     return 0;
 }
